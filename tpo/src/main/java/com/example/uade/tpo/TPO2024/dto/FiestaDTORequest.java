cb0cba9ee6b86a668d8f725a6645dffb0292806c
SebastianSosa12 <106325281+SebastianSosa12@users.noreply.github.com>
2024-07-02 16:48:56 -0300

    OrdenDeCompra funcionando

--- tpo/src/main/java/com/example/uade/tpo/TPO2024/dto/FiestaDTORequest.java
+++ tpo/src/main/java/com/example/uade/tpo/TPO2024/dto/FiestaDTORequest.java
@@ -1,48 +1,9 @@
 package com.example.uade.tpo.TPO2024.dto;
 
-import com.example.uade.tpo.TPO2024.entity.Fiesta;
-import com.example.uade.tpo.TPO2024.entity.OrdenDeCompra;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
 public class FiestaDTORequest {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne
-    @JoinColumn(name = "orden_id", nullable = false)
-    @JsonBackReference
-    private OrdenDeCompra ordenDeCompra;
-
-    @OneToOne
-    @JoinColumn(name = "fiesta_id", referencedColumnName = "id")
-    private Fiesta fiesta;
-
-    @Column
     private String name;
-
-    @Column
     private int cantidadEntradas;
-
-    @Column
-    private int montoParcial;
 }
--- tpo/src/main/java/com/example/uade/tpo/TPO2024/entity/FiestaAsociada.java
+++ tpo/src/main/java/com/example/uade/tpo/TPO2024/entity/FiestaAsociada.java
@@ -1,47 +1,47 @@
-package com.example.uade.tpo.TPO2024.dto;
+package com.example.uade.tpo.TPO2024.entity;
 
 import com.example.uade.tpo.TPO2024.entity.Fiesta;
 import com.example.uade.tpo.TPO2024.entity.OrdenDeCompra;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-//@Data
-//@Builder
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Entity
-public class FiestaDTO {
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "fiestasasociadas")
+public class FiestaAsociada {
 
-    // @Id
-    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // @ManyToOne
-    // @JoinColumn(name = "orden_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "orden_id", nullable = false)
+    @JsonBackReference
     private OrdenDeCompra ordenDeCompra;
 
-    // @OneToOne
-    // @JoinColumn(name = "fiesta_id", referencedColumnName = "id")
-    // private Fiesta fiesta;
-
-    // @Column
+    @Column
     private String name;
 
-    // @Column
+    @Column
     private int cantidadEntradas;
 
-    // @Column
-    private double montoParcial; // PRECIO * CANTIDAD
-
+    @Column
+    private double montoParcial;
 }
